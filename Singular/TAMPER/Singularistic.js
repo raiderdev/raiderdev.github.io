8b3914f9fb005171ba608986d811b0c932801297
Austin-Bollinger <austinbollinger@email.grcc.edu>
2016-10-20 11:33:34 -0400

    Nice work team!
    Nice work team!

--- Singular/TAMPER/Singularistic.js
+++ Singular/TAMPER/Singularistic.js
@@ -6,6 +6,8 @@
 // @description  Automating IT Support tasks.
 // @match        https://mail.grcc.edu/*
 // @match        https://csprod.grcc.edu/*
+// @match        https://contactcenter1.grcc.edu:8445/*
+// @match        https://itsupport.grcc.edu:8443/*
 // @grant        metadata
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -18,6 +20,7 @@
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
+//GroupWise Web Mail
 if (document.location.href.search("1USERNAME") !== -1){
     var x = window.location.href;
     var breakup = x.split("USERNAME",2);
@@ -29,6 +32,7 @@ if (document.location.href.search("1USERNAME") !== -1){
     $(".submitButton").trigger("click");
 }
 
+//CSPROD
 if (document.location.href.search("2USERNAME") !== -1){
     var x = window.location.href;
     var breakup = x.split("USERNAME",2);
@@ -39,4 +43,33 @@ if (document.location.href.search("2USERNAME") !== -1){
     $('#userid').val(username2);
     $('#pwd').val(password2);
     $('#login > div > div > div:nth-child(6) > div > input').trigger("click");
+}
+
+//Cisco Finesse
+if (document.location.href.search("3USERNAME") !== -1){
+    var x = window.location.href;
+    var breakup = x.split("USERNAME",2);
+    //The user should enter their username lowercase.
+    var username3 = breakup[1];
+    var breakup = x.split("PASSWORD",2);
+    var password3 = breakup[1];
+    var breakup = x.split("EXTENSION",2);
+    var extension3 = breakup[1];
+    $('#username').val(username3);
+    $('#password').val(password3);
+    $('#extension').val(extension3);
+    $('#signin-button').trigger("click");
+}
+
+//eHelpDesk Ticket System
+if (document.location.href.search("4USERNAME") !== -1){
+    var x = window.location.href;
+    var breakup = x.split("USERNAME",2);
+    //CSPROD required uppercase characters.
+    var username4 = breakup[1].toUpperCase();
+    var breakup = x.split("PASSWORD",2);
+    var password4 = breakup[1];
+    $('#loginId').val(username4);
+    $('body > form > table:nth-child(1) > tbody > tr > td > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(5) > td:nth-child(2) > input[type="password"]').val(password4);
+    $('body > form > table:nth-child(1) > tbody > tr > td > table > tbody > tr:nth-child(3) > td > input[type="submit"]').trigger("click");
 }
